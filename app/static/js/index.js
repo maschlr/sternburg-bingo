8af016b9b9b1e2d0bf109349b11809e35ae9c898
Marten Schlueter <marten.schlueter@gmail.com>
2016-05-01 14:15:47 +0200

    added alert for matche with an option to remove all matched caps

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -22,6 +22,15 @@ function IndexViewModel() {
     return $.ajax(request);
   }
 
+  self.removeAll = function(match) {
+    // TODO: Error: caps with a count of zero are removed
+    var matchedCaps = self.caps().filter(function(cap) {return match.numbers.indexOf(cap.number) > -1});
+    for (var i in matchedCaps) {
+      self.remove(matchedCaps[i]);
+    }
+    self.checkBingos()
+  }
+
   self.add = function(cap) {
     self.ajax(self.capsURI + "/add/" + cap.number, 'PUT').done(function(data) {
       var i = self.caps.indexOf(cap);
@@ -32,7 +41,7 @@ function IndexViewModel() {
 
   self.remove = function(cap) {
     var cc = cap.count();
-    if (cc < 1) {jj
+    if (cc < 1) {
       alert("Negative Kronkorkenanzahl nicht zulässig. Du trinkst zuviel oder zu wenig!")
     } else {
       self.ajax(self.capsURI + "/remove/" + cap.number, 'PUT').done(function(data) {
@@ -44,13 +53,18 @@ function IndexViewModel() {
   }
 
   self.checkBingos = function() {
+    for (var i=0; i<self.matches().length; i++) {
+      self.matches.pop();
+    }
     var allBingos = self.allBingos();
-    debugger;
     var ownedNumbers = self.caps().filter(function(cap) {return cap.count() > 0})
                                   .map(function(cap) {return cap.number});
     for (var i=0; i<allBingos.length; i++) {
-      if (allBingos[i].map(function(field) {return ownedNumbers.indexOf(field) > -1}).every()) {
-        self.matches.push({index: i, numbers: allBingos[i]});
+      // Array.every() is not working for me so we map and filter and check if we have 5 matches
+      if (allBingos[i].map(function(field) {return ownedNumbers.indexOf(field) > -1})
+                      .filter(function(item) {return item}).length === 5) {
+        var match = {index: i, numbers: allBingos[i]};
+        self.matches.push(match);
       }
     }
     if (self.matches().length > 0) {
